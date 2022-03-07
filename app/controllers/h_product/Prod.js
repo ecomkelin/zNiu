07c6f0ff22c657473074766b916027d5a8c9e13f
kelin <ecom.feng@gmail.com>
2022-03-07 15:20:53 +0100

    12

--- app/controllers/h_product/Prod.js
+++ app/controllers/h_product/Prod.js
@@ -256,15 +256,15 @@ exports.ProdPut = async(req, res) => {
 				obj.weight = parseFloat(obj.weight);
 				if(!isNaN(obj.weight)) Prod.weight = obj.weight;
 			}
-			if(obj.price_regular) {
+			if(obj.price_regular || obj.price_cost == 0) {
 				obj.price_regular = parseFloat(obj.price_regular);
 				if(!isNaN(obj.price_regular)) Prod.price_regular = obj.price_regular;
 			}
-			if(obj.price_sale) {
+			if(obj.price_sale || obj.price_cost == 0) {
 				obj.price_sale = parseFloat(obj.price_sale);
 				if(!isNaN(obj.price_sale)) Prod.price_sale = obj.price_sale;
 			}
-			if(obj.price_cost) {
+			if(obj.price_cost || obj.price_cost == 0) {
 				obj.price_cost = parseFloat(obj.price_cost);
 				if(!isNaN(obj.price_cost)) Prod.price_cost = obj.price_cost;
 			}
