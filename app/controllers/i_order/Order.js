2d1249a942f7872d05773e99f453ba4fab3e2a46
ecomkelin <ecomkelin@gmail.com>
2022-07-30 21:32:06 +0200

    85

--- app/controllers/i_order/Order.js
+++ app/controllers/i_order/Order.js
@@ -282,13 +282,11 @@ exports.OrderPost = async(req, res) => {
 								obj_OrderSku.price = Sku.price_sale;
 							}
 						}
-						console.log(111, obj_OrderSku);
-
 					} else {
 						if(isNaN(obj_OrderSku.price)) return MdFilter.jsonFailed(res, {message: `obj_OrderSku.price 必须为数字`});
 						obj_OrderSku.price_regular = obj_OrderSku.price_sale = obj_OrderSku.price = parseFloat(obj_OrderSku.price);
 						// obj_OrderSku.weight = isNaN(obj_OrderSku.weight) ? 0: parseFloat(obj_OrderSku.weight);
-						console.log(222, obj_OrderSku);
+						obj_OrderSku.attrs = String(obj_OrderSku.attrs);
 					}
 
 					const _OrderSku = new OrderSkuDB(obj_OrderSku);
--- app/models/order/OrderSku.js
+++ app/models/order/OrderSku.js
@@ -14,7 +14,7 @@ const dbSchema = new Schema({
 	status: Number,										// enum: ConfOrder;
 	// 基本信息
 	Sku: {type: ObjectId, ref: "Sku"},					// [post写, put只读]
-	attrs: String,										// [只读 相对 Sku]
+	attrs: String,										// [只读 相对 Sku] // description
 	weight: Float, 										// [只读] 	订单的货品重量
 	price_regular: Float,								// [只读 相对 Sku] 加入购物车时的原价 客户confirm时的原价
 	price_sale: Float,									// [只读 权限 Client相对 Sku] 加入购物车时的交易价格 客户confirm时的交易价格
