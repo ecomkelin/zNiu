f968d867ce0286fa71e58e28291ffa3070efb390
ecomkelin <ecomkelin@gmail.com>
2022-12-20 10:53:10 +0100

    3.16-1

--- app/controllers/i_order/Order_post_Client.js
+++ app/controllers/i_order/Order_post_Client.js
@@ -34,7 +34,7 @@ exports.OrderPost_CartProd = async(req, res) => {
 	try {
 		const payload = req.payload;
 		// 判断 基本参数 是否正确
-		const {CartProds} = req.body.obj;
+		const {CartProds} = req.body;
 		if(!CartProds) return MdFilter.jsonFailed(res, {message: "请传递正确的obj数据"});
 		const obj_Order = {};
 
